df8f9fb578ca0ed37956c4286f84f20ac50d6564
Satya Prakash <143164106+satya643@users.noreply.github.com>
2025-08-12 23:57:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const bands = [
+	'The Plot in You',
+	'The Devil Wears Prada',
+	'Pierce the Veil',
+	'Norma Jean',
+	'The Bled',
+	'Say Anything',
+	'The Midway State',
+	'We Came as Romans',
+	'Counterparts',
+	'Oh, Sleeper',
+	'A Skylit Drive',
+	'Anywhere But Here',
+	'An Old Dog'
+];
+
+// Function to remove leading articles
+function stripArticle(bandName) {
+	return bandName.replace(/^(a |an |the )/i, '').trim();
+}
+
+// Sort bands ignoring articles
+const sortedBands = bands.sort((a, b) => stripArticle(a).localeCompare(stripArticle(b)));
+
+// Display in the UL
+document.getElementById('band').innerHTML = sortedBands
+	.map(band => `<li>${band}</li>`)
+	.join('');
